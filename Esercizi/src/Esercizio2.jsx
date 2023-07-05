a0dbeb24780356647180dc80c8e75cd5ed2985dd
Antonino Alampi <Alampiantonino99@gmail.com>
2023-07-05 20:25:15 +0200

    Fixato esercizio 2

--- Esercizi/src/Esercizio2.jsx
+++ Esercizi/src/Esercizio2.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export class Welcome extends React.Component {
   render() {
-    return <span>Welcome {this.props.name}</span>;
+    return <p>Welcome {this.props.name}!</p>;
   }
 }
