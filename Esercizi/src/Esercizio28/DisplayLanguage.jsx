aef258a87e40041d392f6d2b7b3cfbc72571eb73
Antonino Alampi <Alampiantonino99@gmail.com>
2023-07-14 18:14:41 +0200

    Sistemato esercizio

--- Esercizi/src/Esercizio28/DisplayLanguage.jsx
+++ Esercizi/src/Esercizio28/DisplayLanguage.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { LanguageContext } from "./LanguageContext";
 
-const strings = {
-  en: {
-    greet: "Hello",
-  },
-  it: {
-    greet: "Ciao",
-  },
-};
-
 export class DisplayLanguage extends React.Component {
   render() {
     return (
       <div>
         <LanguageContext.Consumer>
           {(language) => {
-            return <h1>{strings[language].greet}</h1>;
+            return <h1>{language}</h1>;
           }}
         </LanguageContext.Consumer>
       </div>
