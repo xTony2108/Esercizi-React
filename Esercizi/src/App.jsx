5615f56f432770df4a511aa114356f50e5e36de0
Antonino Alampi <Alampiantonino99@gmail.com>
2023-07-17 20:32:51 +0200

    Completato esercizio 45

--- Esercizi/src/App.jsx
+++ Esercizi/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CounterFunc3 } from "./Esercizio42/CounterFunc3";
 import { Route, Routes } from "react-router-dom";
 import { Page1 } from "./pages/Page1";
+import { ShowGithubUser } from "./Esercizio45/ShowGithubUser";
 
 class App extends React.Component {
   render() {
@@ -9,10 +10,12 @@ class App extends React.Component {
       <>
         <Routes>
           <Route path="/" element={<Page1 />} />
-          <Route path="/counter" element={<CounterFunc3 />} />
+          <Route path="counter" element={<CounterFunc3 />} />
+          <Route path="users/:username" element={<ShowGithubUser />} />
         </Routes>
       </>
     );
+    //Riga 14 è l'esercizio 45 => React Router - Add the `users/:username` path
   }
 }
 
